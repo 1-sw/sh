ff5ef6fc9182d315a1750b543ef27703d6e4d520
1-sw <cppa619@gmail.com>
2021-01-18 13:23:27 -0500

    Add gitignore + config

---
+++ vk/main.py
@@ -0,0 +1,14 @@
+import config
+import vk_api
+
+
+
+num = config.number
+pwd = config.pwd
+
+vk_session = vk_api.VkApi(num,pwd)
+vk_session.auth()
+
+vk = vk_session.get_api()
+
+print(vk.wall.post(message='Hello world!'))
