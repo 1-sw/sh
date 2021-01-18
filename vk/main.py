bf613d6eef6ac8b113711d5bc4770fc8bc65c0ea
1-sw <cppa619@gmail.com>
2021-01-18 14:02:15 -0500

    Refresh files

--- vk/main.py
+++ vk/main.py
@@ -1,11 +1,17 @@
-import modules.VK
+#BASE
+import modules
 import config
 import vk_api
+
+#CLASSES
+from modules import VK
+
+
 
 num = config.number
 pwd = config.pwd
 
-vk = VK(num,pwd)
+vk = VK.VK(num,pwd)
 vk = vk.GET_API()
 
 print(vk.wall.post(message='Hello world!'))
--- vk/modules/VK.py
+++ vk/modules/VK.py
@@ -3,15 +3,15 @@ import vk_api
 class VK:
 
   def __init__(self,num,pwd):
-    print(__class__+" : inited!")
+    print(str(__class__)+" : inited!")
     self.num = num
     self.pwd = pwd
-    SESSION  = vk_api.VkApi(num,pwd)
+    self.SESSION  = vk_api.VkApi(num,pwd)
     self.SESSION.auth()
     self.API = self.SESSION.get_api()
 
   def GET_API(self):
-    print(__class__+"->GET_API : called")
+    print(str(__class__)+"->GET_API : called")
     return self.API
 
 
